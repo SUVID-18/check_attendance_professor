ad7a53fcae9b635ebdc2aad08d5730814ffa1167
dongminii <h990309@naver.com>
2023-04-29 18:01:17 +0900

    이미지 에셋. 교수용 자신의 과목표시 페이지 초안.

--- lib/view/subjects_page.dart
+++ lib/view/subjects_page.dart
@@ -7,7 +7,167 @@ class SubjectPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO(front): 과목 목록 화면 구성하기
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('과목 목록'),
+      ),
+
+      ///카드 사용해서 클릭시 상세정보로 넘어감.
+      ///총 4개
+
+
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: [
+          Card(
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              ///한번 클릭시
+              onTap: () {
+                Navigator.push(
+                  context,
+                  ///다음 화면으로 이동
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        Scaffold(
+                          appBar: AppBar(
+                            title: Text('상세 정보'),
+                          ),
+                          body: Center(
+                            child: Text('1학년 전공필수'),
+                          ),
+                        ),
+                  ),
+                );
+              },
+
+              ///카드안에 교과목 + 간단한 사진 등록했음.
+              /// 이미지는 어차피 자기 과목들만 뜨니깐 교실 앞 호수 같은거 이용해도 될듯.
+              child: SizedBox(
+                width: 200,
+                height: 200,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("디지털논리회로"),
+                    SizedBox(height: 20),
+                    Image.asset('asset/image/JST.jpg'),
+                  ],
+                ),
+              ),
+              ),
+            ),
+
+
+
+          Card(
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        Scaffold(
+                          appBar: AppBar(
+                            title: Text('상세 정보'),
+                          ),
+                          body: Center(
+                            child: Text('2학년 전공필수'),
+                          ),
+                        ),
+                  ),
+                );
+              },
+              child: SizedBox(
+                width: 200,
+                height: 200,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("컴퓨터 구조"),
+                    SizedBox(height: 20),
+                    Image.asset('asset/image/JST.jpg'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          Card(
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        Scaffold(
+                          appBar: AppBar(
+                            title: Text('상세 정보'),
+                          ),
+                          body: Center(
+                            child: Text('2학년 전공선택'),
+                          ),
+                        ),
+                  ),
+                );
+              },
+              child: SizedBox(
+                width: 200,
+                height: 200,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("디지털회로 및 실습"),
+                    SizedBox(height: 20),
+                    Image.asset('asset/image/JST.jpg'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+
+          Card(
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        Scaffold(
+                          appBar: AppBar(
+                            title: Text('상세 정보'),
+                          ),
+                          body: Center(
+                            child: Text('4학년 전공필수'),
+                          ),
+                        ),
+                  ),
+                );
+              },
+              child: SizedBox(
+                width: 200,
+                height: 200,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("졸업프로젝트"),
+                    SizedBox(height: 20),
+                    Image.asset('asset/image/JST.jpg'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
