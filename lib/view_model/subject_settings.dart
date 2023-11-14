3c26934b63b9ea3fae43265c76cb0f3fe84ec658
sun30812 <sun30812@naver.com>
2023-11-14 20:19:27 +0900

    불필요한 코드 및 주석 제거

--- lib/view_model/subject_settings.dart
+++ lib/view_model/subject_settings.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 /// 과목 설정을 위한 기능을 구현한 뷰모델로, 과목 출결 유효시간 변경 및 과목 삭제를 수행하는 메서드가 내장되어 있다.
 class SubjectSettingsViewModel {
-  // 싱글톤 패턴 부분
   
   /// 출결 유효시간을 설정하기 위한 int 타입의 변수.
   ///
@@ -20,8 +19,6 @@ class SubjectSettingsViewModel {
 
   /// 뷰모델을 최초로 호출할 경우 객체를 생성하는 private 생성자이다.
   SubjectSettingsViewModel._privateConstructor({required this.subjectID});
-  
-  // ----------------------
 
   /// 과목 설정 화면을 띄울 때 현재 설정된 과목 출결 유효시간을 데이터베이스로부터 가져오는 메서드이다.
   Stream<QuerySnapshot<Map<String, dynamic>>> getValidTime() {
